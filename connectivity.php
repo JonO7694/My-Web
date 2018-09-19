7770f9fa5e671e471ffc2dc6fdcdf5417292f73f
JonO7694 <41658571+JonO7694@users.noreply.github.com>
2018-09-19 15:26:33 +1200

    Update connectivity.php

--- connectivity.php
+++ connectivity.php
@@ -9,8 +9,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$sql = "INSERT INTO UserLogin (FirstName, LastName, Password )
-VALUES ('John', 'Doe', '@example.com')";
+$name = $_POST["user"];
+echo $name;
+$sql = "INSERT INTO UserLogin (LastName, Password )
+VALUES ($name, 'test123')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
